87b0d166194a22741875301eaeb7372228602e62
syakimovich <s0yakimovich@gmail.com>
2022-07-11 18:36:58 +0300

    spring initializr initial project

---
+++ src/test/java/com/github/syakimovich/chessserver/ChessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syakimovich.chessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
