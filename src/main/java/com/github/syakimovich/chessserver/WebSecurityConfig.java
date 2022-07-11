531425032746f54785e777746f0dc99aa9159969
syakimovich <s0yakimovich@gmail.com>
2022-07-11 18:37:00 +0300

    login and signup

---
+++ src/main/java/com/github/syakimovich/chessserver/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.github.syakimovich.chessserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(a -> a.antMatchers("/signup", "/static/**").permitAll().anyRequest().authenticated())
+                .formLogin(a -> a.loginPage("/login").permitAll().defaultSuccessUrl("/", true));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
